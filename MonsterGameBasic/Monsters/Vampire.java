5e4b77495e3d1199c9108986807e07f81740e15c
Gabriel <gabspereira178@gmail.com>
2024-11-19 15:19:13 +0000

    initial commit

---
+++ MonsterGameBasic/Monsters/Vampire.java
@@ -0,0 +1,30 @@
+package minderaExercices.MonsterGame.MonsterGameBasic.Monsters;
+
+import minderaExercices.MonsterGame.MonsterGameBasic.Random;
+
+public class Vampire extends Monster {
+
+	public Vampire() {
+		super();
+		health = 100;
+		damage = 18;
+		name = "Vampire";
+	}
+
+	@Override
+	public void specialAbility() {
+		bite();
+	}
+
+	//to utilize in game I must return if vampire will bite or not so the action will occur in game
+	public void bite() {
+		int changeOfBite = Random.generateBiteChange();
+
+		//25% change of biting, also checking if health is near 85 ensure that health doesn't go above is max health
+		if (changeOfBite <= 25 && health <= 85) {
+			int healthBack = 15;
+			health += healthBack;
+		}
+	}
+
+}
